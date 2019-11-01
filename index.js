40231748dc64d1e65cc088c86fd08407801c0250
Martti Laine <martti@codeclown.net>
2019-11-01 17:39:54 +0200

    /dump-env

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
 
+app.get('/dump-env', (req, res) => {
+  res.send(Object.keys(process.env).map(key => `${key}=${process.env[key]}`).join('\n'));
+});
+
 app.listen(8084, () => {
   console.log('coyo-test-app-node-server listening at http://localhost:8084');
 });
