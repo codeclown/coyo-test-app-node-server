d6d57498c00e8cd609f86552da21cfe00ed227c0
Martti Laine <martti@codeclown.net>
2019-12-07 17:14:48 +0700

    Postgres test

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 
+const { Client } = require('pg');
+const client = new Client({
+  connectionString: process.env.DB_URL
+});
+
 const app = express();
 
 app.get('/', (req, res) => {
@@ -10,6 +15,13 @@ app.get('/dump-env', (req, res) => {
   res.send(Object.keys(process.env).map(key => `${key}=${process.env[key]}`).join('\n'));
 });
 
-app.listen(8084, () => {
-  console.log('coyo-test-app-node-server listening at http://localhost:8084');
+app.get('/pg-test', async (req, res) => {
+  const hello = await client.query('SELECT $1::text as message', ['Hello world from Postgres!']);
+  res.send(hello.rows[0].message);
+});
+
+client.connect().then(() => {
+  app.listen(8084, () => {
+    console.log('coyo-test-app-node-server listening at http://localhost:8084');
+  });
 });
